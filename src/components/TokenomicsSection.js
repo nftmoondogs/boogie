a26334809b00ccce2049a7e9f1dc9f18007785f6
nftmoondogs <nftmoondogs@gmail.com>
2024-09-26 15:59:25 +0530

    newcommit

--- src/components/TokenomicsSection.js
+++ src/components/TokenomicsSection.js
@@ -21,12 +21,12 @@ const TokenomicsSection = () => {
           <img src="https://i.ibb.co/jZhx9fF/pepe.gif" alt="DIELA PreSale" className="rounded-lg shadow-xl transform transition duration-500 hover:scale-110" />
         </div>
         <div>
-        <h2 className="text-3xl font-bold mb-6 text-fuchsia-600">The chase is on, little degen. Dash into the safety of your stablecoins, or better yet, dash away from the screen. But it won't help. The game is just beginning, and $BOOGIE’s shadow looms large. Radium's liberation is my resurrection, and when I rise, the whole Tron blockchain will echo with the sound of your heartbeats... and your screams."
+        <h2 className="text-3xl font-bold mb-6 text-fuchsia-600">The chase is on, little degen. Dash into the safety of your stablecoins, or better yet, dash away from the screen. But it won't help. The game is just beginning, and $BOOGIE’s shadow looms large. Radium's liberation is my resurrection, and when I rise, the whole Solana blockchain will echo with the sound of your heartbeats... and your screams."
 
 </h2>
 
           <p className="mb-4">
-            $BOOGIE PreSale will take place on sunpump.meme, BEAN WILL MAKE JEETS CRY
+            $BOOGIE PreSale will take place on Pump.Fun💊, BEAN WILL MAKE JEETS CRY
           </p>
           <h3 className="text-2xl font-bold mb-4 text-fuchsia-600">Radium's liberation is my resurrection</h3>
           <ul className="list-disc list-inside space-y-2">
